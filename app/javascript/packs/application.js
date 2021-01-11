f06d8c54deab77081da0674e36f8159d689fd9bf
Lonneke Vuur <lonnekevuur@saemor.com>
2021-01-11 09:39:02 +0100

    Add home page and add bootstrap 4

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "stylesheets/site"
+
+require.context('../images', true)
 
 Rails.start()
 Turbolinks.start()
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  root 'pages#main'
   resources :reviews, only: [:new, :create]
 end
